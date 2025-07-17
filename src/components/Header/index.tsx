7d5def304b0046ab1d129dc1e8efda364fbee928
fabriciopontesf <pontesfabricio546@gmail.com>
2025-07-17 00:29:06 -0300

    feat: adiciona estilos ao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <header>
-            <Link href="/">Receitas Deliciosas</Link>
-
-            <nav>
-                <Link href="/">
-                    Início
-                </Link>
-
-                <Link href="/receitas">
-                    Receitas
-                </Link>
-            </nav>
+        <header className="w-full bg-white text-black py-4">
+            <div className="flex justify-between container mx-auto">
+                <Link className="text-xl font-bold hover:scale-105 transition-all" href="/">Receitas Deliciosas</Link>
+                <nav className="flex gap-6">
+                    <Link className="hover:text-orange-500 transition-colors" href="/">
+                        Início
+                    </Link>
+                    <Link className="hover:text-orange-500 transition-colors" href="/receitas">
+                        Receitas
+                    </Link>
+                </nav>
+            </div>
         </header>
     )
 }
