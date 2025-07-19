b88011c2d52ef4e07ce04fd78c428fb6942fe667
fabriciopontesf <pontesfabricio546@gmail.com>
2025-07-18 22:05:00 -0300

    feat: estrutura da secao receitas em destaque

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -11,6 +12,20 @@ export default function Home() {
             <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
             <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
               Ver todas as receitas
+            </Link>
+          </div>
+        </section>
+
+        {/* seção receitas em destaque */}
+        <section>
+          <div className="container mx-auto">
+            <h2>Receitas em destaque</h2>
+
+            {/* cards de receitas */}
+
+            <Link href="/receitas">
+              Ver todas as receitas
+              <ChevronRight />
             </Link>
           </div>
         </section>
